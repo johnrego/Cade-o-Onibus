fb0198ab76a534ed4bd2a40d467076996f14d999
Jefferson John <jefferson.john.rego@gmail.com>
2018-01-23 22:01:04 -0300

    Atualização

--- gps/gps.ino
+++ gps/gps.ino
@@ -1,4 +1,5 @@
 #include "SoftwareSerial.h"
+#include "math.h"
     
 // Cria uma serial nas portas 2 (RX) e 3 (TX)
 SoftwareSerial gps(2 , 3);
@@ -35,9 +36,14 @@ void decodifica(String line){
    * decimal = 06.12853
    */
   Serial.print("Latitude: ");
-  Serial.print(line.substring(17, 27));
-  Serial.print(" ");
-  Serial.println(line.substring(28, 29));
+  char hlat = line.charAt(28);
+  float lat = (line.substring(17, 19).toInt())+((line.substring(19, 27).toFloat())/60);
+  if(hlat == 'S'){
+    Serial.println(lat*(-1), 5);
+  }
+  else{
+    Serial.println(lat, 5); 
+  }
   /*
    * 03812.3879 E is the DDDMM.MMMM format
    * 038 --> degrees
@@ -48,10 +54,21 @@ void decodifica(String line){
    * decimal = 77.20646
    */
   Serial.print("Longitude: ");
-  Serial.print(line.substring(30, 41));
-  Serial.print(" ");
-  Serial.println(line.substring(42, 43));
+  char hlon = line.charAt(42);
+  float lon = (line.substring(30, 33).toInt())+((line.substring(33, 41).toFloat())/60);
+  if(hlon == 'W'){
+    Serial.println(lon*(-1), 5);
+  }
+  else{
+    Serial.println(lon, 5); 
+  }
   Serial.print("Numero de Satelites: ");
-  Serial.println(line.substring(46, 48));
+  Serial.println(line.substring(46, 48).toInt());
+  /*
+  //Medindo distancia até a UFERSA (-6.1066548,-38.1840574). Distancia = 3540
+  double dist = ((acos(cos((90-lat)*M_PI/180)*cos((90-(-6.1066548))*M_PI/180)+sin((90-lat)*M_PI/180)*sin((90-(-6.1066548))*M_PI/180)*cos(fabs(((360+lon)*M_PI/180)-((360+(-38.1840574))*M_PI/180)))))*6371.004)*1000;
+  Serial.print("Distancia ate a UFERSA: ");
+  Serial.println(dist, 2);
+  */
 }
 
